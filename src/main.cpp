517e968bda9f69fc2184d0a72f5450b61eca671b
Dandenet <daniildyakov2001@yandex.ru>
2020-08-27 22:20:38 +0700

    include glad

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
+#include <iostream>
 int main(void)
 {
     GLFWwindow* window;
@@ -19,11 +20,15 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    if(!gladLoadGL())
+        return -1;
+
     /* Loop until the user closes the window */
+    glClearColor(0, 1, 0, 1);
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-       // glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
